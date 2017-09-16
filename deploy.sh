8e5c05325f87258cc11c7b09dad19fd1cf311e87
Jeb Beich <jebbeich@gmail.com>
2017-09-16 12:24:15 -0500

    WIP package SleepLambda as a step function

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ lein uberjar
 mkdir tmp
 cd tmp
 cp ../target/sleep-dev-instance-0.1.0-standalone.jar .
-# We can't refer to ../template.yml below, or we'll upload everything in the
+# We can't refer to ../template.yaml below, or we'll upload everything in the
 # root folder, rather than tmp.
-cp ../template.yml .
+cp ../template.yaml .
 jar xf sleep-dev-instance-0.1.0-standalone.jar
 # Remove the stuff we don't need.
-rm -rf target src buildspec.yml project.clj pom.xml sleep-dev-instance-0.1.0-standalone.jar
+rm -rf target src buildspec.yaml project.clj pom.xml sleep-dev-instance-0.1.0-standalone.jar
 # Why use cloudformation here, rather than lambda directly? Becaues we can
 # reuse the build artifacts w/ the CI pipeline.
-aws cloudformation package --template-file template.yml --s3-bucket com-jebbeich --output-template template-output.json
+aws cloudformation package --template-file template.yaml --s3-bucket com-jebbeich --output-template template-output.json
 aws cloudformation deploy --template-file template-output.json --stack-name sleep-dev-instance-lambda-stack --capabilities CAPABILITY_IAM
 
--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
-aws lambda invoke --function-name SleepDevInstance results.json
-cat results.json
-rm results.json
+# TODO can we fetch the arn here, or pass the name as an argument?
+
+result=$(aws stepfunctions start-execution --state-machine-arn arn:aws:states:us-east-1:803068370526:stateMachine:SleepStateMachine-S07UMBPKCWLA)
+
+echo "START EXECUTION..."
+echo $result
+
+executionarn=$(echo $result | python -c "import json,sys;obj=json.load(sys.stdin);print obj['executionArn'];")
+
+echo "EXECUTION-ARN: $executionarn"
+
+result=$(aws stepfunctions describe-execution --execution-arn $executionarn)
+
+echo "EXECUTION RESULTS..."
+echo $result
+
+
